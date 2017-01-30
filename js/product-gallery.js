0778f41f5eda898120d31bccd9225d9e9a3ce6fe
dmitriy-morozov <dimokin777@mail.ru>
2017-01-30 13:49:01 +0200

    add production version

--- js/product-gallery.js
+++ js/product-gallery.js
@@ -1,18 +1,1 @@
- $(document).ready(function() {
-     $("#thumbs img").hover(function() {
-         $('#largeImage').attr('src', $(this).attr('src').replace());
-     });
-     var imgSwap = [];
-     $("#thumbs img").each(function() {
-         imgUrl = this.src.replace();
-         imgSwap.push(imgUrl);
-     });
-
-
-     $(imgSwap).preload();
- });
- $.fn.preload = function() {
-     this.each(function() {
-         $('<img/>')[0].src = this;
-     });
- }
+$(document).ready(function(){$("#thumbs img").hover(function(){$("#largeImage").attr("src",$(this).attr("src").replace())});var r=[];$("#thumbs img").each(function(){imgUrl=this.src.replace(),r.push(imgUrl)}),$(r).preload()}),$.fn.preload=function(){this.each(function(){$("<img/>")[0].src=this})};
